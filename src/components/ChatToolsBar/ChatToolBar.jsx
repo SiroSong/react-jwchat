0ccb92736d7f69e08454e264938659d37565149e
Ultraman Leo <277702281@qq.com>
2021-01-10 16:36:19 +0800

    up

--- src/components/ChatToolsBar/ChatToolBar.jsx
+++ src/components/ChatToolsBar/ChatToolBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import style from './style.module.css'
-import EmojiPopover from '../EmojiPopover/EmojiPopover'
+// import EmojiPopover from '../EmojiPopover/EmojiPopover'
 export default class ChatToolBar extends Component {
   static propTypes = {
     tools: PropTypes.array,
@@ -15,7 +15,7 @@ export default class ChatToolBar extends Component {
   render() {
     return (
       <div className={style.content}>
-        <EmojiPopover onSelect={this.props.onEmojiSelect} />
+        {/* <EmojiPopover onSelect={this.props.onEmojiSelect} /> */}
         {this.props.tools.map((tool) => tool)}
       </div>
     )
