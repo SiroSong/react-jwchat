16d7c87e465875c3af1f991caa5aa1ec69a9bbc0
雷欧奥特曼 <277702281@qq.com>
2021-05-08 13:46:48 +0800

    up 更新部分逻辑

--- lib/src/components/ChatHeader/ChatHeader.d.ts
+++
@@ -1,5 +0,0 @@
-declare function ChatHeader(props: any): JSX.Element;
-declare namespace ChatHeader {
-    var propTypes: {};
-}
-export default ChatHeader;
--- lib/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import ContactItem from './components/ContactItem/ContactItem.jsx';
-import ContactList from './components/ContactList/ContactList.jsx';
-import Chat from './components/Chat/Chat.jsx';
-export { ContactItem, Chat, ContactList };
