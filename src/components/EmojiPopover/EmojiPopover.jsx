041c9e88fc57eea0b6cd85e4f3bdadf05523a710
宋金源 <songjinyuan-jk@360shuke.com>
2021-06-29 19:53:46 +0800

    up emoji弹窗hook重构

--- src/components/EmojiPopover/EmojiPopover.jsx
+++ src/components/EmojiPopover/EmojiPopover.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import style from './style.module.css'
 import { toClasses } from '../../utils/toClass'
@@ -92,55 +92,51 @@ const emojiList = [
   '🤕',
 ]
 
-export default class EmojiPopover extends Component {
-  state = {
-    visible: false,
-  }
+export default function EmojiPopover({ onSelect = () => {} }) {
+  const [visible, setVisible] = useState(false)
 
-  componentDidMount() {
-    addEventListener('click', (e) => {
-      if (e.target.getAttribute('datatype') === 'emoji') {
-        this.switchEmojiModal(true)
-      } else {
-        this.switchEmojiModal(false)
-      }
-    })
-  }
-
-  switchEmojiModal = (vis = null) => {
+  const switchEmojiModal = (vis = null) => {
     if (vis !== null) {
-      this.setState({ visible: vis })
+      setVisible(vis)
     } else {
-      this.setState({ visible: !this.state.visible })
+      setVisible(!visible)
     }
   }
 
-  iconClickHandle = (emoji) => {
-    this.props.onSelect(emoji)
+  const iconClickHandle = (emoji) => {
+    onSelect(emoji)
   }
 
-  render() {
-    return (
-      <div className={style.content}>
-        <div
-          className={style.emoji_wrapper}
-          style={{ display: !this.state.visible && 'none' }}>
-          {emojiList.map((emoji) => (
-            <span
-              onClick={this.iconClickHandle.bind(this, emoji)}
-              className={style.emoji_item}
-              datatype={emoji}
-              key={emoji}>
-              {emoji}
-            </span>
-          ))}
-        </div>
-        <div
-          className={toClasses([style.tool_icon, style.emoji])}
-          datatype="emoji"></div>
+  useEffect(() => {
+    addEventListener('click', (e) => {
+      if (e.target.getAttribute('datatype') === 'emoji') {
+        switchEmojiModal(true)
+      } else {
+        switchEmojiModal(false)
+      }
+    })
+  }, [])
+
+  return (
+    <div className={style.content}>
+      <div
+        className={style.emoji_wrapper}
+        style={{ display: !visible && 'none' }}>
+        {emojiList.map((emoji) => (
+          <span
+            onClick={iconClickHandle.bind(this, emoji)}
+            className={style.emoji_item}
+            datatype={emoji}
+            key={emoji}>
+            {emoji}
+          </span>
+        ))}
       </div>
-    )
-  }
+      <div
+        className={toClasses([style.tool_icon, style.emoji])}
+        datatype="emoji"></div>
+    </div>
+  )
 }
 
 EmojiPopover.propTypes = {
