b5dc2bbd8294cc725a5cb4f2bf330712d7352453
宋金源 <songjinyuan-jk@360shuke.com>
2021-06-30 09:42:11 +0800

    up 重构chattoolbar

--- src/components/ChatToolsBar/ChatToolBar.jsx
+++ src/components/ChatToolsBar/ChatToolBar.jsx
@@ -3,24 +3,23 @@ import PropTypes from 'prop-types'
 import style from './style.module.css'
 import EmojiPopover from '../EmojiPopover/EmojiPopover'
 import ImgPopover from '../ImgPopover/ImgPopover'
-export default class ChatToolBar extends Component {
-  static propTypes = {
-    tools: PropTypes.array,
-    onEmojiSelect: PropTypes.func,
-    onImage: PropTypes.func.isRequired,
-  }
 
-  static defaultProps = {
-    tools: [],
-  }
+export default function ChatToolBar({
+  tools = [],
+  onEmojiSelect = () => {},
+  onImage = () => {},
+}) {
+  return (
+    <div className={style.content}>
+      <EmojiPopover onSelect={onEmojiSelect} />
+      <ImgPopover onImage={onImage} />
+      {tools.map((tool) => tool)}
+    </div>
+  )
+}
 
-  render() {
-    return (
-      <div className={style.content}>
-        <EmojiPopover onSelect={this.props.onEmojiSelect} />
-        <ImgPopover onImage={this.props.onImage} />
-        {this.props.tools.map((tool) => tool)}
-      </div>
-    )
-  }
+ChatToolBar.propTypes = {
+  tools: PropTypes.array,
+  onEmojiSelect: PropTypes.func,
+  onImage: PropTypes.func.isRequired,
 }
