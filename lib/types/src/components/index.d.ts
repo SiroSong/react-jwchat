e87e670ac765e49b95b9db2e3dc3886d5482ab32
雷欧奥特曼 <sirosong.95@gmail.com>
2021-09-30 16:19:04 +0800

    up gitinnore

--- lib/types/src/components/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Chat from './Chat/Chat';
-import ContactItem from './ContactItem/ContactItem';
-import ContactList from './ContactList/ContactList';
-export { ContactItem, Chat, ContactList };
