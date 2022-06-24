04228fde9e393864c8c9590615d57d8849a24d9f
siro <sirosong@yeah.net>
2022-06-24 11:42:41 +0800

    add doc

---
+++ config/rollup.config.doc.ts
@@ -0,0 +1,26 @@
+import path from 'path'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import serve from 'rollup-plugin-serve'
+import postcss from 'rollup-plugin-postcss'
+import cleaner from 'rollup-plugin-cleaner'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+export default {
+  input: './doc/doc.tsx',
+  output: {
+    file: path.resolve('./doc/dist/doc.js'),
+    format: 'iife',
+    sourcemap: false,
+  },
+  plugins: [
+    cleaner({ targets: [path.resolve('./doc/dist/doc.js')] }),
+    commonjs(),
+    postcss({ plugins: [] }),
+    resolve({ extensions }),
+    babel({ exclude: 'node_modules/**', extensions, babelHelpers: 'bundled' })
+  ],
+}
---
+++ doc/fake.ts
@@ -0,0 +1,211 @@
+import { IContact } from '../src/components/Chat/Chat'
+
+export const contactList = [
+  {
+    id: 1,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Fizz.png',
+    nickname: '菲兹',
+    message: '你抓不到我！',
+    date: '02-07',
+  },
+  {
+    id: 8,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Khazix.png',
+    nickname: '卡兹克',
+    message: '❓',
+    date: '02-07',
+  },
+  {
+    id: 7,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Jayce.png',
+    nickname: '杰斯',
+    message: '吃我一炮',
+    date: '02-07',
+  },
+  {
+    id: 16,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Jinx.png',
+    nickname: '金克丝',
+    message: '起飞🤣 ！！！',
+    date: '02-07',
+  },
+  {
+    id: 2,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Vladimir.png',
+    nickname: '弗拉基米尔',
+    message: '让我嘬一口...',
+    date: '02-07',
+  },
+  {
+    id: 3,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/XinZhao.png',
+    nickname: '阿信',
+    message: '寒芒，我是寒芒！',
+    date: '02-07',
+  },
+  {
+    id: 4,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Caitlyn.png',
+    nickname: '凯特琳',
+    message: '超级瞄准已部署。',
+    date: '02-07',
+  },
+  {
+    id: 5,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Teemo.png',
+    nickname: '提莫',
+    message: '这波团我想活下来',
+    date: '02-07',
+  },
+  {
+    id: 6,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Volibear.png',
+    nickname: '沃利贝尔',
+    message: '熊大呢？',
+    date: '02-07',
+  },
+
+  {
+    id: 9,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Rengar.png',
+    nickname: '雷恩加尔',
+    message: '您已被该玩家禁言',
+    date: '02-07',
+  },
+  {
+    id: 11,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Chogath.png',
+    nickname: '科加斯',
+    message: '您已被该玩家禁言',
+    date: '02-07',
+  },
+  {
+    id: 12,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Amumu.png',
+    nickname: '阿木木',
+    message: '您已被该玩家禁言这是我的第一条信这是我的第一条信',
+    date: '02-07',
+  },
+  {
+    id: 13,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Draven.png',
+    nickname: '德莱文',
+    message: '您已被该玩家禁言',
+    date: '02-07',
+  },
+  {
+    id: 14,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Lucian.png',
+    nickname: '卢锡安',
+    message: '您已被该玩家禁言',
+    date: '02-07',
+  },
+  {
+    id: 15,
+    avatar: '//game.gtimg.cn/images/lol/act/img/champion/Diana.png',
+    nickname: '黛安娜',
+    message: '您已被该玩家禁言',
+    date: '02-07',
+  },
+]
+
+export const contact: IContact = {
+  id: 9527,
+  avatar: '//game.gtimg.cn/images/lol/act/img/champion/Khazix.png',
+  nickname: '卡兹克',
+  desc: '看我点水就完了',
+}
+
+export const my = {
+  id: 1234,
+  avatar: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+  nickname: 'sirosong',
+  desc: 'carry大神',
+}
+
+export const messageList = [
+  {
+    _id: 'ecbb310507ce9c7d3bd57eca046a80f3',
+    date: 1610016423,
+    user: {
+      id: 1234,
+      avatar: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+      nickname: 'sirosong',
+      desc: '这是我的第一条信息',
+    },
+    message: { type: 'text', content: '打野你会玩吗？' },
+  },
+  {
+    _id: 'f3386f6dcf4e69a8bd51a0d688d05580',
+    date: 1610016470,
+    user: {
+      id: 9527,
+      avatar: '//game.gtimg.cn/images/lol/act/img/champion/Khazix.png',
+      nickname: '卡兹克',
+      desc: '这是我的第一条信息',
+    },
+    message: { type: 'text', content: '❓ ' },
+  },
+  {
+    _id: 'f3386f6dcf4e69a8bd51a0d688d0e980',
+    date: 1610016470,
+    user: {
+      id: 1234,
+      avatar: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+      nickname: 'sirosong',
+      desc: '这是我的第一条信息',
+    },
+    message: { type: 'text', content: '对面下路压线那么深你不来？' },
+  },
+  {
+    _id: '75b5bde8f3b9ef7aa9b704492cb28baf',
+    date: 1610016580,
+    user: {
+      id: 1234,
+      avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC_aiJfKgKic97zGdWHJ-G1WiiEDHgPo8gSg&usqp=CAU',
+      nickname: 'sirosong',
+      desc: '这是我的第一条信息',
+    },
+    message: { type: 'text', content: '敌我打野差距👎 ' },
+  },
+  {
+    _id: '75b5bde8f3b9ef7aa9b70449244444cb28baf',
+    date: 1610016880,
+    user: {
+      id: 1234,
+      avatar: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+      nickname: 'sirosong',
+      desc: '这是我的第一条信息',
+    },
+    message: {
+      type: 'image',
+      content:
+        'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fyouimg1.c-ctrip.com%2Ftarget%2Ftg%2F035%2F063%2F726%2F3ea4031f045945e1843ae5156749d64c.jpg&refer=http%3A%2F%2Fyouimg1.c-ctrip.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623381551&t=1ec0b6fdbfc4ba9f149281c1d5f8e370',
+    },
+  },
+  {
+    _id: '75b5bde8f3b9ef7aa9b4492cb28baf',
+    date: 1610016580,
+    user: {
+      id: 1234,
+      avatar: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+      nickname: 'sirosong',
+      desc: '这是我的第一条信息',
+    },
+    message: { type: 'text', content: '敌我打野差距👎 ' },
+  },
+  {
+    _id: '75b5bde8f3b9ef7aa904492cb28baf',
+    date: 1610016880,
+    user: {
+      id: 9527,
+      avatar: '//game.gtimg.cn/images/lol/act/img/champion/Khazix.png',
+      nickname: '卡兹克',
+      desc: '这是我的第一条信息',
+    },
+    message: {
+      type: 'image',
+      content: '//game.gtimg.cn/images/lol/act/a20201103lmpwjl/icon-ht.png',
+    },
+  },
+]
