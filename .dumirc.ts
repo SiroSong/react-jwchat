09042748ae76c2cf9ae277f635abc6ceea8b0067
siro <sirosong@yeah.net>
2023-08-01 15:39:41 +0800

    up path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "dumi"
 
 export default defineConfig({
+  base: "/react-jwchat/",
+  publicPath: "/react-jwchat/",
   outputPath: "docs-dist",
   themeConfig: {
     name: "RC",
     logo: "/logo.png",
   },
   favicons: ["/logo.png"],
-  // resolve: {
-  //   docDirs: ["home"],
-  // },
 })
